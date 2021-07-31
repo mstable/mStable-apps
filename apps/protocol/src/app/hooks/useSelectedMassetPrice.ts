f65f1028df96fcc740669bdb935cebb304ca2da5
James Lefrere <james.lefrere@mstable.org>
2021-07-31 06:49:28 +0200

    Use Eth mainnet WBTC address

--- apps/protocol/src/app/hooks/useSelectedMassetPrice.ts
+++ apps/protocol/src/app/hooks/useSelectedMassetPrice.ts
@@ -1,8 +1,16 @@
+import { ChainIds, getNetwork } from '@apps/base/context/network'
 import { useFetchPriceCtx } from '@apps/base/context/prices'
 import { FetchState, useSelectedMassetState } from '@apps/hooks'
+
+const {
+  addresses: {
+    ERC20: { WBTC },
+  },
+} = getNetwork(ChainIds.EthereumMainnet)
 
 export const useSelectedMassetPrice = (): FetchState<number> => {
   const massetState = useSelectedMassetState()
   const useFetchPrice = useFetchPriceCtx()
-  return useFetchPrice(massetState?.address)
+  const wbtcPrice = useFetchPrice(WBTC)
+  return massetState?.token?.symbol === 'mBTC' ? wbtcPrice : { value: 1 }
 }
