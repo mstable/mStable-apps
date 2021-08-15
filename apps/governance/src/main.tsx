983295e1c764142d3976de6567d2ce0ff6276d99
James Lefrere <james.lefrere@mstable.org>
2021-08-15 16:04:32 +0100

    Add StakingProvider

--- apps/governance/src/main.tsx
+++ apps/governance/src/main.tsx
@@ -3,12 +3,15 @@ import * as ReactDOM from 'react-dom'
 import { BaseLayout, BaseProviders } from '@apps/base'
 
 import { GovernanceApp } from './app'
+import { StakingProvider } from './app/context/StakingProvider'
 
 ReactDOM.render(
   <BaseProviders>
-    <BaseLayout>
-      <GovernanceApp />
-    </BaseLayout>
+    <StakingProvider>
+      <BaseLayout>
+        <GovernanceApp />
+      </BaseLayout>
+    </StakingProvider>
   </BaseProviders>,
   document.getElementById('root'),
 )
