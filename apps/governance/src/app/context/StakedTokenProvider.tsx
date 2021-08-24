0c860430a293b342a4a82a0694f3a6e73e53e319
James Lefrere <james.lefrere@mstable.org>
2021-08-24 22:50:23 +0200

    StakedTokenProvider
    - Add a provider to select a StakedToken and query it

---
+++ apps/governance/src/app/context/StakedTokenProvider.tsx
@@ -0,0 +1,59 @@
+import React, { createContext, Dispatch, FC, SetStateAction, useMemo, useState } from 'react'
+
+import { useAccount } from '@apps/base/context/account'
+import { useApolloClients } from '@apps/base/context/apollo'
+import { providerFactory, createUseContextFn, useBlockPollingSubscription } from '@apps/hooks'
+import { useStakedTokenLazyQuery } from '@apps/artifacts/graphql/staking'
+
+import { useStakingQuery } from './StakingProvider'
+
+interface State {
+  options: { [address: string]: { asset: { address: string }; icon: { symbol: string } } }
+  selected?: string
+}
+
+const stateCtx = createContext<State>(null as never)
+const dispatchCtx = createContext<Dispatch<SetStateAction<string>>>(null as never)
+
+export const StakedTokenProvider: FC = ({ children }) => {
+  const [selected, setSelected] = useState<string>()
+  const stakingQuery = useStakingQuery()
+
+  return providerFactory(
+    dispatchCtx,
+    { value: setSelected },
+    providerFactory(
+      stateCtx,
+      {
+        value: useMemo<State>(() => {
+          const data = stakingQuery.data?.stakedTokens ?? []
+          const options = Object.fromEntries(
+            data.map(({ id: address, stakingToken: { symbol } }) => [address, { icon: { symbol }, asset: { address, symbol } }]),
+          )
+
+          // Default to Staked MTA (not BPT)
+          return { selected: selected ?? data.find(st => st.stakingToken.symbol === 'MTA')?.id, options }
+        }, [selected, stakingQuery.data]),
+      },
+      children,
+    ),
+  )
+}
+
+export const useStakedToken = createUseContextFn(stateCtx)
+
+export const useSetStakedToken = createUseContextFn(dispatchCtx)
+
+export const useStakedTokenQuery = () => {
+  const clients = useApolloClients()
+  const account = useAccount()
+
+  const { selected } = useStakedToken()
+
+  const options = useMemo(
+    () => ({ client: clients.staking, variables: { id: selected, account: account ?? '', hasAccount: !!account } }),
+    [account, clients, selected],
+  )
+
+  return useBlockPollingSubscription(useStakedTokenLazyQuery, options, !selected)
+}
--- apps/governance/src/main.tsx
+++ apps/governance/src/main.tsx
@@ -3,14 +3,17 @@ import * as ReactDOM from 'react-dom'
 import { BaseLayout, BaseProviders } from '@apps/base'
 
 import { GovernanceApp } from './app'
+import { StakedTokenProvider } from './app/context/StakedTokenProvider'
 import { StakingProvider } from './app/context/StakingProvider'
 
 ReactDOM.render(
   <BaseProviders>
     <StakingProvider>
-      <BaseLayout>
-        <GovernanceApp />
-      </BaseLayout>
+      <StakedTokenProvider>
+        <BaseLayout>
+          <GovernanceApp />
+        </BaseLayout>
+      </StakedTokenProvider>
     </StakingProvider>
   </BaseProviders>,
   document.getElementById('root'),
