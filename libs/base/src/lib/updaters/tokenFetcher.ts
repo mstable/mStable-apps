44627408958b10149fd75af9d7b622b2e6afa850
James Lefrère <james.lefrere@mstable.org>
2021-09-13 18:43:28 +0200

    Add Kovan support

--- libs/base/src/lib/updaters/tokenFetcher.ts
+++ libs/base/src/lib/updaters/tokenFetcher.ts
@@ -1,14 +1,15 @@
 import { useEffect, useMemo } from 'react'
 import { useAllTokensQuery as useAllTokensProtocolQuery } from '@apps/artifacts/graphql/protocol'
 import { useFeederTokensQuery } from '@apps/artifacts/graphql/feeders'
+import { useTokensQuery as useStakingTokensQuery } from '@apps/artifacts/graphql/staking'
 
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { useApolloClients } from '@apps/base/context/apollo'
-import { useNetwork } from '../context/NetworkProvider'
+
 import { useTokensDispatch } from '../context/TokensProvider'
 
 export const TokenFetcher = (): null => {
   const clients = useApolloClients()
-  const network = useNetwork()
   const { setFetched } = useTokensDispatch()
 
   const options = useMemo(() => {
@@ -19,20 +20,22 @@ export const TokenFetcher = (): null => {
       feeders: {
         client: clients.feeders,
       },
-      // staking: {
-      //   client: clients.staking,
-      // },
+      staking: {
+        client: clients.staking,
+      },
       // stakingRewards: {
       //   client: clients.stakingRewards,
       // },
     }
-  }, [clients, network])
+  }, [clients])
 
   const protocolQuery = useAllTokensProtocolQuery(options.protocol)
   const feedersQuery = useFeederTokensQuery(options.feeders)
+  const stakingQuery = useStakingTokensQuery(options.staking)
 
   const protocolFetched = protocolQuery.data?.tokens ?? []
   const feedersFetched = feedersQuery.data?.feederPools ?? []
+  const stakingFetched = stakingQuery.data?.tokens ?? []
 
   // Sub/unsub when the list of tokens changes from what's subscribed.
   useEffect(() => {
@@ -42,8 +45,11 @@ export const TokenFetcher = (): null => {
     if (feedersFetched.length > 0) {
       setFetched(feedersFetched.flatMap(fp => [fp.token, fp.fasset]))
     }
+    if (stakingFetched.length > 0) {
+      setFetched(stakingFetched)
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [protocolFetched.length, feedersFetched.length])
+  }, [protocolFetched.length, feedersFetched.length, stakingFetched.length])
 
   return null
 }
