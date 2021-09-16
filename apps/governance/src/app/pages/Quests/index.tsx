5da9e210041fd36d8a28d200adca0393def6b97a
chrisjgf <chrisjgf@gmail.com>
2021-09-16 17:58:55 +0100

    2 + 2 = 4 - 3 = 1

--- apps/governance/src/app/pages/Quests/index.tsx
+++ apps/governance/src/app/pages/Quests/index.tsx
@@ -1,11 +1,21 @@
 import React, { FC } from 'react'
+import styled from 'styled-components'
 
 import { GovernancePageHeader } from '../../components/GovernancePageHeader'
 import { Meta8Console } from './Meta8Console'
 
+const Formula = styled.span`
+  ${({ theme }) => theme.mixins.numeric};
+  background: ${({ theme }) => theme.color.background[1]};
+  font-size: 0.75rem;
+  color: ${({ theme }) => theme.color.bodyAccent};
+`
+
 export const Quests: FC = () => (
   <div>
-    <GovernancePageHeader title="Quests" subtitle="Completing quests boosts your voting power" />
+    <GovernancePageHeader title="Quests" subtitle="Voting power is determined by quest completions and overall time staked">
+      <Formula>voting_power = balance * quest_multiplier * time_multiplier</Formula>
+    </GovernancePageHeader>
     <Meta8Console />
   </div>
 )
