b7dd6119f81c75d6ccb8da49a5272f8e7b5fbbd7
James Lefrère <james.lefrere@mstable.org>
2021-09-22 12:30:00 +0100

    Update nx

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/protocol',
-    '<rootDir>/libs/artifacts',
-    '<rootDir>/libs/base',
-    '<rootDir>/libs/bigdecimal',
-    '<rootDir>/libs/formatters',
-    '<rootDir>/libs/transaction-manifest',
-    '<rootDir>/libs/hooks',
-    '<rootDir>/libs/react-utils',
-    '<rootDir>/libs/types',
-    '<rootDir>/libs/quick-maths',
-    '<rootDir>/libs/components',
-    '<rootDir>/libs/assets',
-    '<rootDir>/libs/masset-provider',
-    '<rootDir>/libs/data-provider',
-    '<rootDir>/apps/governance',
-  ],
-}
+const { getJestProjects } = require('@nrwl/jest')
+
+module.exports = { projects: getJestProjects() }
