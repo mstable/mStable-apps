106ffc4f6f796e563a4c197a2903f39adbe0ea59
James Lefrère <james.lefrere@mstable.org>
2021-10-06 12:13:04 +0200

    blockNow by chainId
    - Keep local state for blockNow by chain ID

--- libs/base/src/lib/context/BlockProvider.tsx
+++ libs/base/src/lib/context/BlockProvider.tsx
@@ -14,14 +14,18 @@ interface BlockNumbers {
   block7d?: number
 }
 
+interface State {
+  [chainId: number]: number | undefined
+}
+
 const ctx = createContext<BlockNumbers>(null as never)
 
 const date7d = subDays(Date.now(), 7)
 const date24h = subHours(Date.now(), 24)
 
 export const BlockProvider: FC = ({ children }) => {
-  const [blockNow, setBlockNow] = useState<MaybeBlockNumber>()
-  const network = useNetwork()
+  const [blockNow, setBlockNow] = useState<State>({})
+  const { chainId, blockTime } = useNetwork()
 
   const provider = useProvider()
   const idle = useIsIdle()
@@ -33,25 +37,21 @@ export const BlockProvider: FC = ({ children }) => {
       // Only set the new block number when the user is active
       // getBlockNumber apparently returns a string
       provider?.getBlockNumber().then(latest => {
-        setBlockNow(latest ? parseInt(latest as unknown as string, 10) : undefined)
+        const value = latest ? parseInt(latest as unknown as string, 10) : undefined
+        setBlockNow({ ...blockNow, [chainId]: value })
       })
     }
-  }, network.blockTime)
+  }, blockTime)
 
-  return (
-    <ctx.Provider
-      value={useMemo(
-        () => ({
-          blockNow,
-          block7d: blockTimes[0]?.number ? parseInt(blockTimes[0]?.number as unknown as string) : undefined,
-          block24h: blockTimes[1]?.number ? parseInt(blockTimes[1]?.number as unknown as string) : undefined,
-        }),
-        [blockNow, blockTimes],
-      )}
-    >
-      {children}
-    </ctx.Provider>
-  )
+  const value = useMemo(() => {
+    return {
+      blockNow: blockNow[chainId],
+      block7d: blockTimes[0]?.number ? parseInt(blockTimes[0]?.number as unknown as string) : undefined,
+      block24h: blockTimes[1]?.number ? parseInt(blockTimes[1]?.number as unknown as string) : undefined,
+    }
+  }, [blockNow, blockTimes, chainId])
+
+  return <ctx.Provider value={value}>{children}</ctx.Provider>
 }
 
 export const useBlockNow = (): MaybeBlockNumber => useContext(ctx).blockNow
