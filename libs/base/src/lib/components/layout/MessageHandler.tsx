9e46f69668970e73e8ee42cdafd87af385140cbf
chrisjgf <chrisjgf@gmail.com>
2021-11-16 18:36:30 +0000

    Add Olympus Pro message (#92)

--- libs/base/src/lib/components/layout/MessageHandler.tsx
+++ libs/base/src/lib/components/layout/MessageHandler.tsx
@@ -1,32 +1,46 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 
-import { ExternalLink } from '@apps/dumb-components'
 import { MassetConfig } from '@apps/masset-provider'
-
 import { IBannerMessage } from '../../context/BannerProvider'
 
 interface Props {
   recollat(massetConfig: MassetConfig): IBannerMessage
-  polygon(): IBannerMessage
+  graph: IBannerMessage
+  olympus: IBannerMessage
 }
 
 export const MessageHandler: Props = {
-  recollat: (massetConfig: MassetConfig) => {
-    return {
-      title: `${massetConfig.formattedName} is currently undergoing recollateralisation. `,
-      subtitle: `During this time,
-    mAsset functionality will be reduced in order to restore a healthy
-    basket state.`,
-      emoji: '⚠️',
-    }
-  },
-  polygon: () => ({
-    title: 'mStable on Polygon',
-    subtitle: (
-      <div>
-        Bridge bAssets USDC, DAI or USDT via the <ExternalLink href="https://wallet.matic.network/bridge/">Matic Bridge</ExternalLink>.
-      </div>
+  recollat: (massetConfig: MassetConfig) => ({
+    status: 'warning',
+    content: (
+      <p>{massetConfig.formattedName} is currently undergoing recollateralisation. mAsset functionality will be temporarily reduced.</p>
     ),
-    emoji: '⚠️',
   }),
+  graph: {
+    status: 'warning',
+    content: (
+      <p>
+        Our data provider sometimes falls out of sync on Polygon. If you are experiencing issues please check{' '}
+        <a href="https://status.thegraph.com/" target="_blank" rel="noopener noreferrer">
+          here
+        </a>{' '}
+        before our discord
+      </p>
+    ),
+  },
+  olympus: {
+    status: 'info',
+    content: (
+      <p>
+        We have recently launched MTA bonds on Olympus Pro.{' '}
+        <a
+          href="https://medium.com/mstable/mstable-olympus-pro-treasury-bond-program-for-mstables-expansion-c2e6265e4fa9"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more
+        </a>
+      </p>
+    ),
+  },
 }
--- libs/base/src/lib/context/BannerProvider.tsx
+++ libs/base/src/lib/context/BannerProvider.tsx
@@ -1,11 +1,9 @@
-import { ReactNode } from 'react'
+import React, { ReactElement } from 'react'
 import { createStateContext } from 'react-use'
 
 export interface IBannerMessage {
-  title: string
-  subtitle?: ReactNode
-  emoji: string
-  url?: string
+  content: ReactElement
+  status: 'warning' | 'info'
 }
 
 export const [useBannerMessage, BannerProvider] = createStateContext<IBannerMessage | undefined>(undefined)
