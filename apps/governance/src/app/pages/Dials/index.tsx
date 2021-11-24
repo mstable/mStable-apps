4047c00bc3c24000a3d5032275a923a7e2df05ba
chrisjgf <chrisjgf@gmail.com>
2021-11-24 15:02:01 +0000

    Dials route

---
+++ apps/governance/src/app/pages/Dials/index.tsx
@@ -0,0 +1,9 @@
+import { Slider } from '@apps/dumb-components'
+import React, { FC, useState } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+export const Dials: FC = () => {
+  return <Container></Container>
+}
