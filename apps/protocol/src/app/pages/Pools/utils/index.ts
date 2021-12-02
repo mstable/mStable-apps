7c1933d7b0a174f3a3d064d46e48ff70fff2fdc4
chrisjgf <chrisjgf@gmail.com>
2021-12-02 10:08:54 +0000

    60/40 fix
    Third time lucky

--- apps/protocol/src/app/pages/Pools/utils/index.ts
+++ apps/protocol/src/app/pages/Pools/utils/index.ts
@@ -1,9 +1,9 @@
 import type { InputRatios } from '@apps/types'
 import type { BigDecimal } from '@apps/bigdecimal'
 
-export const scaleFassetAmount = (amount?: BigDecimal, ratios?: InputRatios): BigDecimal | undefined => {
+export const scaleFassetAmount = (amount?: BigDecimal, address?: string, ratios?: InputRatios): BigDecimal | undefined => {
   const addresses = ratios && Object.keys(ratios)
   if (!addresses?.length || !amount) return amount
-  const address = addresses.find(k => !!ratios[k])
-  return !!address ? amount.mulRatioTruncate(ratios[address]).setDecimals(18) : amount
+  const match = !!addresses.find(k => k === address && !!ratios[k])
+  return match ? amount.mulRatioTruncate(ratios[address]).setDecimals(18) : amount
 }
