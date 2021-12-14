4a0455caaaadd1ccc80f9914b71c853d63bf2ac9
James Lefrère <james.lefrere@mstable.org>
2021-12-14 16:55:19 +0100

    Apollo network status
    - Add a network status widget for Apollo data sources

---
+++ libs/base/src/lib/context/NotificationsProvider2.tsx
@@ -0,0 +1,19 @@
+// import { FC } from 'react'
+// import { createReducerContext } from 'react-use'
+//
+// interface Notification<T> {
+//   timestamp: number
+//   content: T
+// }
+//
+// interface State {
+//   [type: string]: Notification[]
+// }
+//
+// type Dispatch<T> = {
+//   type: 'ADD',
+//   payload: { timestamp: number, content: T }
+// } | { type: 'FLUSH' } | { type: '' }
+//
+
+export {}
