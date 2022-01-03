8e82d7bf994ceb0b8bf10ba92016a16e41d8ef8b
James Lefrère <james.lefrere@mstable.org>
2022-01-03 15:12:18 +0100

    Modulate type sounds

--- apps/governance/src/app/pages/Quests/Typist.tsx
+++ apps/governance/src/app/pages/Quests/Typist.tsx
@@ -6,7 +6,7 @@ import useSound from 'use-sound'
 import bleep30 from '../../../assets/bleeps_30.mp3'
 
 export const Typist: FC<TypistProps> = ({ children, cursor = { show: false }, stdTypingDelay = 0, startDelay = 0, avgTypingDelay = 8 }) => {
-  const [play] = useSound(bleep30, { volume: 0.05 })
+  const [play] = useSound(bleep30, { volume: 0.05, playbackRate: 0.9 + Math.min(0.2, Math.random()) })
   return (
     <BaseTypist
       startDelay={startDelay}
