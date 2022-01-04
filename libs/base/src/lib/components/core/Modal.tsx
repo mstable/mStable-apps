4da6666e46e00da3c4295ec261c163fb1843d1f6
chrisjgf <chrisjgf@gmail.com>
2022-01-04 10:59:23 +0000

    Fix modal z-index

--- libs/base/src/lib/components/core/Modal.tsx
+++ libs/base/src/lib/components/core/Modal.tsx
@@ -96,7 +96,7 @@ const FixedContainer = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  z-index: 1;
+  z-index: 2;
   background: ${({ theme }) => (theme.isLight ? `rgba(0, 0, 0, 0.5)` : `rgba(0, 0, 0, 0.75)`)};
 
   @supports (backdrop-filter: blur(0.25em)) {
