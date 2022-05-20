48344b29d4031ba486d59c1bd329c15a6ac5fc12
toniocodo <toniocodo@hotmail.com>
2022-05-20 20:17:42 +0200

    fix: update ipfs src path to pinata gateway

--- libs/base/src/lib/components/core/IPFSImg.tsx
+++ libs/base/src/lib/components/core/IPFSImg.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 
 export const IPFSImg: FC<{ uri?: string; alt?: string }> = ({ uri, alt }) => {
   if (!uri) return null
-  const url = uri.startsWith('ipfs://') ? `https://cloudflare-ipfs.com/ipfs/${uri.slice(7)}` : uri
+  // const url = uri.startsWith('ipfs://') ? `https://cloudflare-ipfs.com/ipfs/${uri.slice(7)}` : uri
+  const url = uri.startsWith('ipfs://') ? `https://gateway.pinata.cloud/ipfs/${uri.slice(7)}` : uri
   return <img src={url} alt={alt as string} />
 }
