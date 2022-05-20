ab6a7ef80682fbb870932c368b8500c0ca05fab3
Antoine Codogno <toniocodo@hotmail.com>
2022-05-20 13:56:10 +0200

    154 icons footer size (#158)
    * style: update social icons, normalize size
    
    * style: update social icons, normalize size
    
    * style: resize icons, replace email

--- libs/base/src/lib/components/layout/Footer.tsx
+++ libs/base/src/lib/components/layout/Footer.tsx
@@ -37,9 +37,8 @@ const SocialIcons = styled(Links)`
   }
 
   img {
-    display: block;
-    width: 24px;
-    height: auto;
+    width: 20px;
+    height: 20px;
     opacity: 0.8;
     filter: sepia(0%) saturate(300%) brightness(250%);
   }
