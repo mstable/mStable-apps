a3d5829cc60f005de6352f0c704c1ef6bcc43989
Dimitri <69167058+dimsome@users.noreply.github.com>
2022-05-31 15:11:04 +0700

    fix: ipfs gateway (#169)
    * fix: ipfs gateway
    
    * chore: updated delegates ipfs file
    
    * chore: update ipfs file

--- apps/governance/src/app/context/DelegateeLists.tsx
+++ apps/governance/src/app/context/DelegateeLists.tsx
@@ -38,7 +38,7 @@ export const DelegateeListsProvider: FC = ({ children }) => {
         // TODO reinstate me when on mainnet
         // const ipfsHash = await resolveENSContentHash(ensName, provider)
         // const url = `https://cloudflare-ipfs.com/ipfs/${ipfsHash ? ipfsHash.slice(7) : ''}/`
-        const url = 'https://gateway.pinata.cloud/ipfs/QmXjUHzohugdqsnUTjmaHMgxYFnHEQN8zPrhrK63eS2Tp7'
+        const url = 'https://mstable.infura-ipfs.io/ipfs/QmcVkdBcaaPmWHoCyv2TJvoCMFzyJRjoYeCkBLPQwFrMGy'
 
         const response = await fetch(url)
         const list = (await response.json()) as DelegateeList
