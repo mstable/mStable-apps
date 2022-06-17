70e7a284c0beba40f95fcff0bce2d4a3e14b697c
Dimitri <69167058+dimsome@users.noreply.github.com>
2022-06-18 00:40:03 +0700

    fix: lint-error

--- apps/governance/src/app/pages/Dials/constants.ts
+++ apps/governance/src/app/pages/Dials/constants.ts
@@ -106,8 +106,7 @@ export const DIALS_METADATA: { [dialId: number]: DialMetadata } = Object.freeze(
     network: Networks.Polygon,
     title: 'FRAX fPool',
     color: '#b7e21a',
-    description:
-      'Voting for this dial will direct MTA emissions to the FRAX/mUSD feeder pool on Polygon.',
+    description: 'Voting for this dial will direct MTA emissions to the FRAX/mUSD feeder pool on Polygon.',
     link: 'https://app.mstable.org/#/musd/pools/0xb30a907084ac8a0d25dddab4e364827406fd09f0?network=polygon',
     linkTitle: 'Pool',
   },
