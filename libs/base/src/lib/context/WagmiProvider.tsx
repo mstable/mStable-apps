ab43cb1c791b42730da9cd3fdb66b89a2d1b2b18
toniocodo <toniocodo@hotmail.com>
2022-06-24 14:34:55 +0200

    fix: stakedSignatures

--- libs/base/src/lib/context/WagmiProvider.tsx
+++ libs/base/src/lib/context/WagmiProvider.tsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react'
 import { useIsDarkMode } from '@apps/browser-settings'
 import { rbkDarkTheme, rbkLightTheme } from '@apps/theme'
 import { connectorsForWallets, RainbowKitProvider, wallet } from '@rainbow-me/rainbowkit'
+import { useEffectOnce } from 'react-use'
 import {
   chain,
   configureChains,
@@ -18,6 +19,8 @@ import {
 import { infuraProvider } from 'wagmi/providers/infura'
 import { publicProvider } from 'wagmi/providers/public'
 
+import { useStakeSignatures } from '../hooks'
+import { API_ENDPOINT } from '../utils'
 import { useChainIdCtx } from './NetworkProvider'
 
 import type { FC } from 'react'
@@ -68,14 +71,52 @@ const client = createClient({
 })
 
 const AccountProvider: FC = ({ children }) => {
+  const { data: account } = useWagmiAccount()
   const [, setChainId] = useChainIdCtx()
   const { activeChain } = useNetwork()
+  const [, setStakeSignatures] = useStakeSignatures()
 
   useEffect(() => {
     if (activeChain?.id) {
       setChainId(activeChain.id)
     }
   }, [activeChain?.id, setChainId])
+
+  useEffectOnce(() => {
+    if (process.env.NX_APP_NAME === 'governance') {
+      fetch(`${API_ENDPOINT}/signature`)
+        .then(resp => resp.json())
+        .then(json => {
+          setStakeSignatures(prevSignatures => ({
+            ...prevSignatures,
+            message: json.message,
+          }))
+        })
+        .catch(console.error)
+    }
+  })
+
+  useEffect(() => {
+    if (process.env.NX_APP_NAME === 'governance' && account?.address) {
+      fetch(`${API_ENDPOINT}/signature/${account?.address}`)
+        .then(resp => resp.json())
+        .then(json => {
+          if (json.error) return
+          setStakeSignatures(prevSignatures => {
+            // TODO: I'm getting a weird race condition here with the library, this fix the issue
+            const prevHack = {
+              ...JSON.parse(localStorage.getItem('stakeSignatures') || '{}'),
+              ...prevSignatures,
+            }
+            return {
+              ...prevHack,
+              [account.address ?? '']: json.signature,
+            }
+          })
+        })
+        .catch(console.warn)
+    }
+  }, [account?.address, setStakeSignatures])
 
   return <>{children}</>
 }
