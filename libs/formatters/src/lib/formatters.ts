1e4e318fb52a3eae30fe9509b3efdd48c77d0fbe
Antoine Codogno <toniocodo@hotmail.com>
2023-02-14 11:18:12 +0100

    Fix/add error message (#238)
    * fix: update error formatter to return reason on CALL_EXCEPTION
    
    * fix: make error message more explicit

--- libs/formatters/src/lib/formatters.ts
+++ libs/formatters/src/lib/formatters.ts
@@ -50,6 +50,7 @@ export const humanizeList = (list: string[]): string =>
 interface EthersError extends Error {
   code?: ErrorCode
   error?: Error
+  reason?: string
 }
 
 const sanitizeEthersError = (error: EthersError): string => {
@@ -70,6 +71,13 @@ const sanitizeEthersError = (error: EthersError): string => {
       return 'Replacement transaction underpriced'
     case ErrorCode.TIMEOUT:
       return 'Timeout'
+    case ErrorCode.CALL_EXCEPTION:
+      switch (error?.reason) {
+        case 'Exceeds weight limits':
+          return 'Exceeds weight limits, switch to multi assets redemption'
+        default:
+          return 'Transaction execution error'
+      }
     default:
       break
   }
