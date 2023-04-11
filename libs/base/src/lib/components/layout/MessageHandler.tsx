9beaad23f7b9caa43765c9d3369d709db4544cbc
toniocodo <toniocodo@hotmail.com>
2023-04-11 22:02:54 +0200

    feat: reword and restyle banner

--- libs/base/src/lib/components/layout/MessageHandler.tsx
+++ libs/base/src/lib/components/layout/MessageHandler.tsx
@@ -16,33 +16,24 @@ interface Props {
 }
 
 const Container = styled.div`
-  /* position: fixed;
-  top: 20%;
-  left: 50%;
-  transform: translate(-50%, -50%); */
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  padding: 2rem 1rem 4rem 1rem;
-
-  @media (max-width: 400px) {
-    top: 0;
-    left: 0;
-    width: 1;
-  }
+  padding: 2rem;
 `
 
 const Modal = styled.div`
   padding: 2rem;
   display: flex;
+  max-width: 968px;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
   gap: 1rem;
-  background-color: rgba(44, 48, 78, 0.4);
   backdrop-filter: blur(2px);
   border-radius: 1rem;
-  border: 1px solid #fff6a1;
+  border: 1px solid #fac371;
+  font-size: 1rem;
 `
 
 export const MessageHandler: Props = {
@@ -105,13 +96,14 @@ export const MessageHandler: Props = {
       <Container>
         <Modal>
           <WarningIcon />
+          <p>With the mStable protocol sunset, it's important to withdraw your funds.</p>
           <p>
-            mStable is sunsetting its entire product line, earning interest through the protocol has been disabled and the project’s smart
-            contracts are no longer operated by the mStableDAO.
+            The contracts will always remain live and allow for withdrawals in perpetuity, however this App will be hosted for only 6 months
+            only (replace with date). Or run it locally afterwards.
           </p>
-          <p>It is strongly advised to withdraw all liquidity positions from the protocol. Read more in the official announcement.</p>
+          <p>DIMITRI NEEDS TO WORK ON THE WORDING HERE</p>
           <a href="" target="_blank" rel="noopener noreferrer">
-            Read More
+            Read the Announcement
           </a>
         </Modal>
       </Container>
